9229f14a1ae36896c6b5f4174a3d6798c8a88bd4
Glaucus37 <brunohs23@gmail.com>
2022-08-13 02:50:00 -0500

    v 1.0

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use std::io;
+
+fn c_to_f(&c: &f32) -> f32 {
+    c * 1.8 + 32.
+}
+fn f_to_c(&f: &f32) -> f32 {
+    (f - 32.) / 1.8
+}
+
+fn deg_str(c: &bool) -> String {
+    match c {
+        true => String::from("°C"),
+        false => String::from("°F"),
+    }
+}
+
+fn main() {
+    loop {
+        let deg_c: bool;    
+        loop {
+            println!("\nWhat conversion do you want to do?\n1. °C -> °F\n2.°F -> °C");
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Readline Error.");
+            
+            deg_c = match input.trim().parse() {
+                Ok(num) => {
+                    match num {
+                        1 => true,
+                        2 => false,
+                        _ => {
+                            println!("Not a valid option!");
+                            continue;
+                        }
+                    }
+                },
+                Err(_) => {
+                    println!("Not a number!");
+                    continue;
+                },
+            };
+            break;
+        }
+
+        let temp: f32;
+        loop {
+            let aux = deg_str(&deg_c);
+            println!("\nEnter a temperature in {aux}.");
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Readline error.");
+
+            temp = match input.trim().parse() {
+                Ok(num) => {
+                    num
+                },
+                Err(_) => {
+                    println!("Not a number!");
+                    continue;
+                },
+            };
+
+            break;
+        }
+
+        let temp_new: f32 = match deg_c {
+            true => c_to_f(&temp),
+            false => f_to_c(&temp),
+        };
+
+        let deg_from = deg_str(&deg_c);
+        let deg_to = deg_str(&!deg_c);
+        println!("\n{temp}{deg_from} = {temp_new}{deg_to}");
+
+        println!("\nWould you like to perform another conversion?\n1. Yes\n2. No");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Readline error.");
+        
+        match input.trim().parse() {
+            Ok(num) => {
+                match num {
+                    1 => continue,
+                    2 => {
+                        println!("\nGoodbye!");
+                        break
+                    },
+                    _ => {
+                        println!("Option not recognized; exiting.");
+                        break;
+                    }
+                }
+            },
+            Err(_) => {
+                println!("Readline error; exiting.");
+                break;
+            }
+        }
+    }
+}
